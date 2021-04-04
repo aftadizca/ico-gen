5cb9f67867e44343a268fbe392c02d3069507da8
aftadizca <aftadizca@gmail.com>
2021-04-04 19:29:05 +0700

    work

--- icogen.py
+++ icogen.py
@@ -3,4 +3,12 @@ from PIL import Image
 
 images = [Image.open(x) for x in ["top.png", "test.jpg", "bottom.png"]]
 
+middle_img = images[1].resize((149, 211))
+
 new_img = Image.new("RGBA", (256, 256))
+
+new_img.paste(images[2])
+new_img.paste(middle_img, (31, 16))
+new_img.paste(images[0], (0, 0), images[0])
+
+new_img.save("res.ico")
