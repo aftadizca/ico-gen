7d3890842e37246b073f3bc3785760a40c9d8db8
aftadizca <aftadizca@gmail.com>
2021-04-04 20:23:39 +0700

    done

--- icogen.py
+++ icogen.py
@@ -1,14 +1,24 @@
 from PIL import Image
+import os
+import fnmatch
+
+dir_anime = "/mnt/d/KOLEKSI/NEWANIME"
+pattern = (".jpg", ".png")
 
 
-images = [Image.open(x) for x in ["top.png", "test.jpg", "bottom.png"]]
+for root, _, files in os.walk(dir_anime):
+    for filename in files:
+        if filename.endswith((".jpg", ".png")):
+            img_target = os.path.join(root, filename)
+            print(" - " + root)
+            images = [Image.open(x) for x in ["top.png", img_target, "bottom.png"]]
 
-middle_img = images[1].resize((149, 211))
+            middle_img = images[1].resize((149, 211))
 
-new_img = Image.new("RGBA", (256, 256))
+            new_img = Image.new("RGBA", (256, 256))
 
-new_img.paste(images[2])
-new_img.paste(middle_img, (31, 16))
-new_img.paste(images[0], (0, 0), images[0])
+            new_img.paste(images[2])
+            new_img.paste(middle_img, (31, 16))
+            new_img.paste(images[0], (0, 0), images[0])
 
-new_img.save("res.ico")
+            new_img.save(os.path.join(root, "a.ico"))
