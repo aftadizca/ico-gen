be4afd2fe9d6f79d96b42dbab230d4fb20428625
aftadizca <aftadizca@gmail.com>
2021-04-09 10:08:17 +0700

    auto download image from anilist.co

--- icogen.py
+++ icogen.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import os
 import pathlib
+from query import getImg
 
 pattern = (".jpg", ".png")
 cwd = pathlib.Path(os.path.realpath(__file__)).parent
@@ -9,23 +10,28 @@ dir_anime = "/mnt/d/KOLEKSI/NEWANIME"
 top_img = os.path.join(cwd, "top.png")
 bottom_img = os.path.join(cwd, "bottom.png")
 
-for root, _, files in os.walk(dir_anime):
-    for filename in files:
-        if filename.endswith(pattern):
-            img_target = os.path.join(root, filename)
-            print(" - " + root)
-            images = [Image.open(x) for x in [top_img, img_target, bottom_img]]
 
-            middle_img = images[1].resize((149, 211), Image.LANCZOS)
+def icon_generator(top_img, img_target, bottom_img):
+    images = [Image.open(x) for x in [top_img, img_target, bottom_img]]
+    middle_img = images[1].resize((149, 211), Image.LANCZOS)
+    new_img = Image.new("RGBA", (256, 256))
+    new_img.paste(images[2])
+    new_img.paste(middle_img, (31, 16))
 
-            new_img = Image.new("RGBA", (256, 256))
+    alphaComposite = Image.alpha_composite(new_img, images[0])
+    alphaComposite.save(os.path.join(root, "a.ico"))
 
-            new_img.paste(images[2])
-            new_img.paste(middle_img, (31, 16))
 
-            alphaComposite = Image.alpha_composite(new_img, images[0])
-            alphaComposite.save(os.path.join(root, "a.ico"))
-
-            # new_img.paste(images[0], images[0])
-
-            # new_img.save(os.path.join(root, "a.ico"))
+for root, dirs, files in os.walk(dir_anime):
+    for folder_name in dirs:
+        if folder_name == "1. new":
+            continue
+        else:
+            img_target = os.path.join(root, folder_name, "icon.jpg")
+            if os.path.isfile(img_target):
+                icon_generator(top_img, img_target, bottom_img)
+                print(f"-{os.path.join(root, folder_name):<70} \tIco Already Exist")
+            else:
+                getImg(folder_name, img_target)
+                icon_generator(top_img, img_target, bottom_img)
+                print(f"-{os.path.join(root, folder_name):<70} \tDone")
---
+++ query.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import urllib.request
+import os
+import pathlib
+
+url = "https://graphql.anilist.co"
+query = """query ($search: String){
+  Media(search: $search) {
+    id
+    coverImage {
+      extraLarge
+    }
+    title {
+      romaji
+    }
+  }
+}"""
+
+cwd = pathlib.Path(os.path.realpath(__file__)).parent
+
+
+def getImg(title, path):
+    vars = {"search": title}
+    response = requests.post(url, json={"query": query, "variables": vars})
+    json_data = json.loads(response.text)
+    img_url = json_data["data"]["Media"]["coverImage"]["extraLarge"]
+
+    req = urllib.request.Request(img_url, headers={"User-Agent": "Mozilla/5.0"})
+    opener = urllib.request.urlopen(req)
+    with open(path, "b+w") as f:
+        f.write(opener.read())
+
+    return img_url
