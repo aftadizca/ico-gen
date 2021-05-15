d1471d34bb49991d4d0f24e7d37334129ccd639e
aftadizca <aftadizca@gmail.com>
2021-05-15 17:56:12 +0700

    fix

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ class Config:
         self.config_file = config_file
         with open(config_file, "r", encoding="utf8") as f:
             self.data = load(f.read(), schema)
+        # adding atribute to config
+        for key1 in self.data.data:
+            for key2 in self.data.data[key1]:
+                setattr(self, key1+"_"+key2, self.data.data[key1][key2])
 
     def get(self, key, sub_key):
         return self.data.data[key][sub_key]
@@ -26,5 +30,9 @@ c = Config('config.yaml')
 # # c.set("api", "url", "test")
 # for attr, value in vars(c.data.data).items():
 #     print(attr, value)
-print(c.data.data)
+# for key1 in c.data.data:
+#     for key2 in c.data.data[key1]:
+#         setattr(c, key1+"_"+key2, c.data.data[key1][key2])
+
+print(c.api_url)
 # print(c.get("api", "url"))
